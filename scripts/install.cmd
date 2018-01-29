51d12df2a2fa0d4d400f0bb99152d51c3516b977
Davidz <Davidz@DESKTOP-NMA1I3P>
2018-01-29 20:47:19 -0200

    Trabalhando em auto update

--- scripts/install.cmd
+++ scripts/install.cmd
@@ -3,9 +3,5 @@ SET rootPath="C:\2Taps"
 SET appPath="%rootPath%\RestpayColibriBridge"
 
 cd /d "%appPath%"
-npm install ^
-    aws-iot-device-sdk@2.2.0 ^ 
-    simple-git@1.89.0 ^
-&& ^
-npm install -g node-windows@0.1.14 && ^
-npm link node-windows
+npm install aws-iot-device-sdk@2.2.0 auto-updater@1.0.0 && ^
+npm install -g node-windows@0.1.14 && npm link node-windows
