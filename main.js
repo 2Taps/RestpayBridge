c851fef49212b4290922a5493d2e06f9e2589327
Davidz <Davidz@DESKTOP-NMA1I3P>
2018-01-30 19:34:10 -0200

    Teste de auto update via topic

--- main.js
+++ main.js
@@ -1,95 +1,123 @@
-const { exec } = require('child_process');
-exec('node '+__dirname+'/autoupdate.js', (err, stdout, stderr) => {
-    if (err) { return; } else {
-        console.log(stdout.toString());
-        if(stdout.toString().indexOf('RESTART THE APP!') != -1) {
-            process.exit(); //force app to exit and be restarted by windows service or our custom monitor
-        } else {
-            const http = require("http");
-            const fs = require('fs');
-            const awsIot = require('aws-iot-device-sdk');
-            const EventLogger = require('node-windows').EventLogger;
-            const logger = new EventLogger('Hello World');
-            const appPath = __dirname;
-            const credPath = appPath+'/credentials';
-            var deviceId = devicePkFile = deviceCertFile = '';
-            fs.readdirSync(credPath).forEach(file => {
-                var fileExt = file.split('.').pop();
-                if(file == 'device-id.txt') {
-                   try { deviceId = fs.readFileSync(credPath+'/'+file).toString().trim(); } catch(e) {}
-                } else if(fileExt == 'key' && file.indexOf('private') != -1) {
-                    devicePkFile = file;
-                } else if(fileExt == 'crt') {
-                    deviceCertFile = file;
-                }
-            })
-            if(!deviceId) { throw new Error('- Device Id file not found.'); }
-            if(!devicePkFile) { throw new Error('- Device Pk file not found.'); }
-            if(!deviceCertFile) { throw new Error('- Device Certificate file not found.'); }
+const AutoUpdater = require('auto-updater');
 
-            var globalPublishTopic = 'restpay-prod-colibri-pc-publish';
-            var devicePublishTopic = deviceId+'-publish';
-            var autoUpdateTopic = 'restpay-prod-colibri-pc-update';
-
-            var device = awsIot.device({
-               keyPath: credPath+'/'+devicePkFile,
-              certPath: credPath+'/'+deviceCertFile,
-                caPath: credPath+'/root-CA.pem',
-              clientId: deviceId,
-                  host: 'a3pfvuzbin0ywl.iot.us-east-1.amazonaws.com',
-                  debug: false
-            });
-
-            var bodyChunks = [];
-            var req = reqData = {};
-            var body = publishTopic = publishJson = '';
-
-            device.on('connect', function() {
-                console.log('connected');
-                device.subscribe(deviceId);
-                device.subscribe(autoUpdateTopic);
-            }).on('message', function(topic, payload) {
-                console.log('Message arrived...');
-                console.log(topic);
-                if(topic == deviceId) {
-                    payload = JSON.parse(payload.toString());
-                    console.log(payload);
-                    reqData = payload.colibri_api_req;
-                    req = http.request(
-                        {
-                            method: reqData.method,
-                            hostname: 'localhost',
-                            port: reqData.port,
-                            path: '/'+reqData.version+reqData.uri+'?'+reqData.params,
-                            auth: reqData.username+':'+reqData.password
-                        }, function(res) {
-                            bodyChunks = [];
-                            res.on('data', function(chunk) {
-                                bodyChunks.push(chunk);
-                            }).on('end', function() {
-                                body = Buffer.concat(bodyChunks);
-                                publishJson = JSON.stringify({ 
-                                    id_user: String(payload.id_user),
-                                    timestamp: String(payload.timestamp),
-                                    response: body.toString()
-                                });
-                                publishTopic = (payload.publish_mode == 1)?globalPublishTopic:devicePublishTopic;
-                                console.log('Republishing to topic: '+publishTopic+'...');
-                                device.publish(publishTopic, publishJson);
-                            });
-                        }
-                    );
-                    req.on('error', function(e) {
-                        console.log(e);
-                    });
-                    req.end();
-                } else if(topic == autoUpdateTopic) {
-                    process.exit(); //force app to exit and be restarted by windows service or our custom monitor
-                }
-            }).on('error', function(error) {
-                try { logger.log(error); } catch(error) {}
-                throw new Error(error);
-            });
-        }
-    }
+var autoupdater = new AutoUpdater({
+    pathToJson: '',
+    autoupdate: true,
+    checkgit: false,
+    jsonhost: 'raw.githubusercontent.com',
+    contenthost: 'codeload.github.com',
+    progressDebounce: 0,
+    devmode: false
 });
+
+autoupdater
+.on('git-clone', function() { console.log("You have a clone of the repository. Use 'git pull' to be up-to-date"); })
+.on('check.up-to-date', function(v) { console.info("You have the latest version: " + v); })
+.on('check.out-dated', function(v_old, v) { console.warn("Your version is outdated. " + v_old + " of " + v); })
+.on('update.downloaded', function() { console.log("Update downloaded and ready for install"); })
+.on('update.not-installed', function() { console.log("The Update was already in your folder! It's read for install"); })
+.on('download.start', function(name) { console.log("Starting downloading: " + name); })
+.on('download.progress', function(name, perc) { process.stdout.write("Downloading " + perc + "% \033[0G"); })
+.on('download.end', function(name) { console.log("Downloaded " + name); })
+.on('download.error', function(err) {
+    //try { logger.log(e); } catch(error) {} 
+    console.error(name, e); process.exit();
+})
+.on('error', function(name, e) {
+    //try { logger.log(e); } catch(error) {} 
+    console.error(name, e); process.exit();
+})
+.on('update.extracted', function() {
+    console.log("Update extracted successfully! Restarting..."); process.exit();
+})
+.on('end', function() {
+    console.log("The app is ready to function");
+    
+    const http = require("http");
+    const fs = require('fs');
+    const awsIot = require('aws-iot-device-sdk');
+    //const EventLogger = require('node-windows').EventLogger;
+    //const logger = new EventLogger('Hello World');
+    const appPath = __dirname;
+    const credPath = appPath+'/credentials';
+    var deviceId = devicePkFile = deviceCertFile = '';
+    fs.readdirSync(credPath).forEach(file => {
+        var fileExt = file.split('.').pop();
+        if(file == 'device-id.txt') {
+           try { deviceId = fs.readFileSync(credPath+'/'+file).toString().trim(); } catch(e) {}
+        } else if(fileExt == 'key' && file.indexOf('private') != -1) {
+            devicePkFile = file;
+        } else if(fileExt == 'crt') {
+            deviceCertFile = file;
+        }
+    })
+    if(!deviceId) { throw new Error('- Device Id file not found.'); }
+    if(!devicePkFile) { throw new Error('- Device Pk file not found.'); }
+    if(!deviceCertFile) { throw new Error('- Device Certificate file not found.'); }
+
+    var globalPublishTopic = 'restpay-prod-colibri-pc-publish';
+    var devicePublishTopic = deviceId+'-publish';
+    var autoUpdateTopic = 'restpay-prod-colibri-pc-update';
+
+    var device = awsIot.device({
+       keyPath: credPath+'/'+devicePkFile,
+      certPath: credPath+'/'+deviceCertFile,
+        caPath: credPath+'/root-CA.pem',
+      clientId: deviceId,
+          host: 'a3pfvuzbin0ywl.iot.us-east-1.amazonaws.com',
+          debug: false
+    });
+
+    var bodyChunks = [];
+    var req = reqData = {};
+    var body = publishTopic = publishJson = '';
+
+    device.on('connect', function() {
+        console.log('connected');
+        device.subscribe(deviceId);
+        device.subscribe(autoUpdateTopic);
+    }).on('message', function(topic, payload) {
+        console.log('Message arrived...');
+        console.log(topic);
+        if(topic == deviceId) {
+            payload = JSON.parse(payload.toString());
+            console.log(payload);
+            reqData = payload.colibri_api_req;
+            req = http.request(
+                {
+                    method: reqData.method,
+                    hostname: 'localhost',
+                    port: reqData.port,
+                    path: '/'+reqData.version+reqData.uri+'?'+reqData.params,
+                    auth: reqData.username+':'+reqData.password
+                }, function(res) {
+                    bodyChunks = [];
+                    res.on('data', function(chunk) {
+                        bodyChunks.push(chunk);
+                    }).on('end', function() {
+                        body = Buffer.concat(bodyChunks);
+                        publishJson = JSON.stringify({ 
+                            id_user: String(payload.id_user),
+                            timestamp: String(payload.timestamp),
+                            response: body.toString()
+                        });
+                        publishTopic = (payload.publish_mode == 1)?globalPublishTopic:devicePublishTopic;
+                        console.log('Republishing to topic: '+publishTopic+'...');
+                        device.publish(publishTopic, publishJson);
+                    });
+                }
+            );
+            req.on('error', function(e) {
+                console.log(e);
+            });
+            req.end();
+        } else if(topic == autoUpdateTopic) {
+            process.exit(); //force app to exit and be restarted by windows service or our custom monitor
+        }
+    }).on('error', function(error) {
+        //try { logger.log(error); } catch(error) {}
+        throw new Error(error);
+    });
+});
+// Start checking
+autoupdater.fire('check');
--- windows-service.js
+++ windows-service.js
@@ -2,13 +2,15 @@ var Service = require('node-windows').Service;
 
 // Create a new service object
 var svc = new Service({
-  name:'2Taps Colibri Bridge',
-  description: 'Consulta a api rest local do colibri para o 2Taps app',
-  script: __dirname+'/main.js',
-  nodeOptions: [
-    '--harmony',
-    '--max_old_space_size=4096'
-  ]
+    name:'2Taps Colibri Bridge',
+    description: 'Consulta a api rest local do colibri para o 2Taps app',
+    script: __dirname+'/main.js',
+    wait: 5,
+    grow: 0,
+    nodeOptions: [
+        '--harmony',
+        '--max_old_space_size=4096'
+    ]
 });
 
 // Listen for the "install" event, which indicates the
