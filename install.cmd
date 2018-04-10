349b38aef4466ba8abe13bf72e66f7b5307afc05
Davidz <Davidz@DESKTOP-NMA1I3P>
2018-04-10 10:46:44 -0300

    Suporta a operacoes POST, PUT e DELETE

--- install.cmd
+++ install.cmd
@@ -3,6 +3,8 @@ SET rootPath="C:\2Taps"
 SET appPath="%rootPath%\RestpayBridge"
 
 cd /d "%appPath%"
-npm install aws-iot-device-sdk@2.2.0 auto-updater@1.0.0 && ^
-npm install -g node-windows@0.1.14 && npm link node-windows && ^
+npm install aws-iot-device-sdk@2.2.0 && ^
+npm install auto-updater@1.0.0 && ^
+npm install -g node-windows@0.1.14 && ^
+npm link node-windows && ^
 node windows-service-install.js
